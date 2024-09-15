496ebacdd73025e2f303d803dd48dceb2c553a4f
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-09-15 21:36:29 +0100

    modified contact section and footer styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import NavbarSection from "./_components/section/NavbarSection/NavbarSection";
 import HeroSection from "./_components/section/HeroSection/HeroSection";
 import ProgramSection from "./_components/section/ProgramSection/ProgramSection";
-import Testimonials from "./_components/section/Testimonials/Testimonials";
+// import Testimonials from "./_components/section/Testimonials/Testimonials";
 import ContactSection from "./_components/section/ContactSection/ContactSection";
 import FooterSection from "./_components/section/FooterSection/FooterSection";
 
