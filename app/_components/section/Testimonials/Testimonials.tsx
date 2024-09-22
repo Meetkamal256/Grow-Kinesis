5cb0e58c20720223b4d1a5a434d38206b01e899c
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-09-22 11:49:17 +0100

    modified testimonial component jsx and styling

--- app/_components/section/Testimonials/Testimonials.tsx
+++ app/_components/section/Testimonials/Testimonials.tsx
@@ -1,7 +1,33 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./Testimonials.module.css";
+import { IoIosArrowDropleft } from "react-icons/io";
+import { IoIosArrowDropright } from "react-icons/io";
+import { reviews } from "@/app/data";
 
+type ExpandedReviews = {
+  [key: number]: boolean;
+};
 const Testimonials = () => {
+  const [expandedReviews, setExpandedReviews] = useState<ExpandedReviews>({});
+
+  const handleToggleReview = (id: number) => {
+    // Create a copy of the current expandedReviews state
+    const updatedReviews = { ...expandedReviews };
+
+    // Check if the review with the given ID is currently expanded or not
+    if (updatedReviews[id]) {
+      // If it's already expanded, set it to false (collapse it)
+      updatedReviews[id] = false;
+    } else {
+      // If it's not expanded, set it to true (expand it)
+      updatedReviews[id] = true;
+    }
+
+    // Update the state with the modified expandedReviews object
+    setExpandedReviews(updatedReviews);
+  };
   return (
     <section className="container">
       <h2 className={styles.sectionTitle}>
@@ -11,70 +37,77 @@ const Testimonials = () => {
         Join a community of approximately 500 active users who&apos;ve unlocked
         their potential with Grow Kinesis. Are you ready to be next?
       </p>
+
       <div className={styles.testimonialsContainer}>
         <div className={styles.imageContainer}>
           <Image
             src="/images/video2.png"
             alt="overlay-img"
             width={914}
-            height={444}
+            height={544}
             className={styles.video}
           />
         </div>
+        {reviews.map((review) => (
+          <div
+            key={review.id}
+            className={`${styles.reviewContainer} ${
+              expandedReviews[review.id] ? styles.expanded : ""
+            }`}
+          >
+            <p
+              className={`${styles.reviewParagraph} ${
+                expandedReviews[review.id] ? styles.expanded : ""
+              }`}
+              onClick={() => handleToggleReview(review.id)}
+            >
+              {expandedReviews[review.id]
+                ? review.info
+                : review.info.substring(0, 350) + "..."}
+            </p>
 
-        <div className={styles.reviewContainer}>
-          <p className={styles.reviewParagraph}>
-            &quot;I&apos;ve been working with GrowKinesis for a long while now,
-            and the experience has been nothing short of transformative. Their
-            innovative approach to growth strategies has helped my athleticism
-            reach new heights. The team is incredibly knowledgeable and
-            responsive, always ready to provide insights and support. I
-            appreciate how they tailor their services to meet my sp...
-            {/*ecific needs,
-            making me feel outstanding in the process. I can&apos;t wait to see
-            what the future holds as we continue this journey together! I highly
-            recommend them to anyone looking for top-notch service!&quot; */}
-          </p>
-
-          <div className={styles.info}>
-            <Image
-              src="/images/avatar1.png"
-              alt="avatar1-img"
-              width={48}
-              height={48}
-              className={styles.avatarImg}
-            />
-            <div>
-              <h4 className={styles.name}>Michael Okiki</h4>
-              <p className={styles.profession}>
-                - Professional Basketball Player (National team PG)
-              </p>
+            <div className={styles.info}>
               <Image
-                src="/images/divider.png"
-                alt="divider"
-                width={326}
-                height={2}
+                src="/images/avatar1.png"
+                alt="avatar1-img"
+                width={48}
+                height={48}
+                className={styles.avatarImg}
               />
-              <div className={styles.btnContainer}>
-                <Image
-                  src="/images/rightarrow.png"
-                  alt="right-arrow"
-                  width={34}
-                  height={34}
-                />
-                <Image
-                  src="/images/rightarrow.png"
-                  alt="right-arrow"
-                  width={34}
-                  height={34}
-                />
+              <div>
+                <h4 className={styles.name}>Michael Okiki</h4>
+                <p className={styles.profession}>
+                  - Professional Basketball Player (National team PG)
+                </p>
               </div>
             </div>
+            <Image
+              src="/images/divider.png"
+              alt="divider"
+              width={326}
+              height={2}
+            />
+            <div className={styles.btnContainer}>
+              <IoIosArrowDropleft
+                className={`${styles.arrowIcon} ${styles.leftArrow}`}
+              />
+              <IoIosArrowDropright
+                className={`${styles.arrowIcon} ${styles.rightArrow}`}
+              />
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
 };
 
 export default Testimonials;
+
+// Handle toggling the expanded state of each review
+// const handleToggleReview = (id: number) => {
+//   setExpandedReviews((prev) => ({
+//     ...prev,
+//     [id]: !prev[id], // Toggle the specific review's expanded state
+//   }));
+// };
