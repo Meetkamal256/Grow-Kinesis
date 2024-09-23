50b6e6252917f0d199b0beefe2406934fc2dcb73
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-09-23 23:08:08 +0100

    modified waitListBadge component css styling for mobile

--- app/_components/WaistListBadge/WaitListBadge.tsx
+++ app/_components/WaistListBadge/WaitListBadge.tsx
@@ -2,10 +2,16 @@ import styles from "@/app/_components/WaistListBadge/WaitListBadge.module.css";
 import Image from "next/image";
 
 const WaitListBadge = () => (
-  <div className={styles.badgeContainer}>
+  <div className={`container ${styles.badgeContainer}`}>
     <div className={styles.badgeText}>
       Approx. 500 active users
-      <Image src="/images/avatars.png" alt="avatars" width={72} height={24} className={styles.avatarImg} />
+      <Image
+        src="/images/avatars.png"
+        alt="avatars"
+        width={72}
+        height={24}
+        className={styles.avatarImg}
+      />
       Awaiting launch
     </div>
   </div>
